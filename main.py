55fc51dd35cd1b635886f581fdb0d38a62e8f766
Aki158 <xiaolinh548@gmail.com>
2023-08-27 18:54:57 +0900

    データストリームの形式に修正

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import random
+import sys
+
 
 def main():
-    print('Guess the number game をスタートします')
-    n = int(input('最小値(n)を入力してください : '))
-    m = int(input('最大値(m)を入力してください : '))
-    if n > m:
-        return print('最小値(n)が最大値(m)を超えています!\nゲームを終了します...')
+    sys.stdout.buffer.write(b'Guess-the-number-game Start!\n')
+    
+    # 最小値(n) を入力する
+    sys.stdout.buffer.write(b'Please enter a minimum value (n) : ')
+    sys.stdout.flush()
+    n = int(sys.stdin.buffer.readline())
 
-    selectMode = int(input('難易度を 0 から 2 の間で選択してください\neasy  (入力 : 0)\nmedium(入力 : 1)\nhard  (入力 : 2)\n入力 : '))
+    # 最大値(m) を入力する
+    sys.stdout.buffer.write(b'Please enter a maximum value (m) : ')
+    sys.stdout.flush()
+    m = int(sys.stdin.buffer.readline())
+
+    if n > m:
+        return sys.stdout.buffer.write(b'Minimum (n) exceeds maximum (m)!\nExiting game...')
+
+    # 難易度を選択する
+    sys.stdout.buffer.write(b'Choose a difficulty level between 0 and 2\neasy  (Input : 0)\nmedium(Input : 1)\nhard  (Input : 2)\nInput : ')
+    sys.stdout.flush()
+    selectMode = int(sys.stdin.buffer.readline())
 
     if(selectMode == 0):
         numberOfAttempts = 15
@@ -16,17 +30,25 @@ def main():
     elif(selectMode == 2):
         numberOfAttempts = 5
     else:
-        return print('難易度を設定できませんでした...\nゲームを終了します')
+        return sys.stdout.buffer.write(b'Could not set difficulty...\nExiting game')
 
     randomNum = random.randint(n,m)
 
     for i in range(numberOfAttempts):
-        print(str(i+1) + '/' + str(numberOfAttempts) + '回目')
-        answerNum = int(input(str(n) + ' から ' + str(m) + ' の間で数字を入力してください : '))
+        s1 = 'Time : ' + str(i+1) + '/' + str(numberOfAttempts) + '\n'
+        sys.stdout.buffer.write(bytes(s1,'utf-8'))
+        
+        # 数字を推測する
+        s2 = 'Enter a number between ' + str(n) + ' and ' + str(m) + ' : '
+        sys.stdout.buffer.write(bytes(s2,'utf-8'))
+        sys.stdout.flush()
+        answerNum = int(sys.stdin.buffer.readline())
+        
         if randomNum == answerNum:
-            return print('正解です!\nゲームを終了します〜')
-        print('違います\n')
-    return print('ゲームオーバー\n正解は' + str(randomNum) + 'でした〜')
+            return sys.stdout.buffer.write(b"That's correct!\nEnd the game")
+        sys.stdout.buffer.write(b"That's Incorrect\n")
+    s3 = 'Game over\nThe correct answer was ' + str(randomNum) + '\n'
+    return sys.stdout.buffer.write(bytes(s3,'utf-8'))
 
 
 if __name__ == '__main__':
